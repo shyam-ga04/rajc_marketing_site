c4db54b2fb92cbec0bed6f1bf30242296c460fb7
shyam-ga04 <shyamganeshravichandran4@gmail.com>
2026-03-28 18:46:25 +0530

    feat: The firebase configuration ahs been done

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import './index.css'
-import { router } from './routes'
-import { ThemeProvider } from './lib/theme'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { RouterProvider } from "@tanstack/react-router"
+import "./index.css"
+import { router } from "./routes"
+import { ThemeProvider } from "./lib/theme"
+import "./lib/firebase/config.ts"
 
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById("root")!
 
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
